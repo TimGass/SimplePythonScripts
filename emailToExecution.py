6da6c094f98218154facaf4e43bf08be93c4b7e4
TimGass <timothyagass@gmail.com>
2017-02-15 01:04:14 -0600

    improved emailToExecution drastically

--- emailToExecution.py
+++ emailToExecution.py
@@ -6,8 +6,9 @@ import datetime
 import email
 from email.mime.text import MIMEText
 from dateutil import tz
-from subprocess import call
+import subprocess
 from confidentials import *
+import re
 
 mail = imaplib.IMAP4_SSL('imap.gmail.com')
 mail.login(myEmail, gpass)
@@ -23,28 +24,28 @@ result, data = mail.fetch(latest_email_id, "(RFC822)")
 raw_email = data[0][1].decode("utf-8")
 email_message = email.message_from_string(raw_email)
 recieved = email_message['Received']
-if abs(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc) - datetime.datetime.strptime(recieved[recieved.find(";")+2:len(recieved)-6], '%a, %d %b %Y %H:%M:%S %z').astimezone(tz.gettz("America/Chicago"))) < datetime.timedelta(seconds=60):
+if abs(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc) - datetime.datetime.strptime(recieved[recieved.find(";")+2:len(recieved)-6], '%a, %d %b %Y %H:%M:%S %z').astimezone(tz.gettz("America/Chicago"))) < datetime.timedelta(seconds=60000):
     if email_message.is_multipart():
         msg = email_message.get_payload()[0].as_string()
     else:
         msg = email_message.get_payload().as_string()
+    #remove extra spaces that exist, for some reason
     msg = msg[msg.find("\n")+1:len(msg)]
     msg = msg[msg.find("\n")+1:len(msg)]
-    firstLineIndex = msg.find("\n")
-    if msg[0:firstLineIndex] == executionString:
-        secondLineIndex = msg.find("\n", firstLineIndex+1)
-        thirdLineIndex = msg.find("\n", secondLineIndex+1)
-        print(msg[firstLineIndex+1:secondLineIndex])
-        try:
-            call(msg[firstLineIndex+1:secondLineIndex], shell=True)
-        except OSError:
-            string = "Command not input command properly! Could not run."
-            error = MIMEText(string)
-            error['Subject'] = "Command Error"
-            error['From'] = myEmail
-            error['To'] = phone
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login(myEmail, gpass)
-            server.sendmail(myEmail, [phone], error.as_string())
-            server.quit()
+    msgArray = []
+    msgArray = re.split('\n', msg, flags=re.A|re.M)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(myEmail, gpass)
+    for i in range(1, len(msgArray)-1, 2):
+        if msgArray[0] == executionString:
+            try:
+                output = subprocess.check_output([ msgArray[i], msgArray[i+1]])
+            except subprocess.CalledProcessError as error:
+                error = MIMEText(str(error))
+                error['Subject'] = "Command Error"
+                error['From'] = myEmail
+                error['To'] = phone
+                server.sendmail(myEmail, [phone], error.as_string())
+                print(error)
+    server.quit()
